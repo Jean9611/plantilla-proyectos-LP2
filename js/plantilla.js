8bf461f13fc04d6ddf8fcbd0d4cc902577ecccf0
Jean9611 <jruizg96.11@gmail.com>
2022-04-18 20:09:18 -0600

    Plantilla inicial

---
+++ js/plantilla.js
@@ -0,0 +1,8 @@
+
+function funcion_demo(){
+    document.getElementById('lblResultado').innerHTML = 'Este texto se escribió desde la funcion_demo';
+}
+
+function click_boton(){
+    funcion_demo();
+}
