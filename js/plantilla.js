03b6e9187be13edef04b6701946a8d543c96c205
Jean9611 <jruizg96.11@gmail.com>
2022-05-24 20:04:26 -0600

    Avances de la clase #2

--- js/plantilla.js
+++ js/plantilla.js
@@ -1,8 +1,28 @@
 
-function funcion_demo(){
-    document.getElementById('lblResultado').innerHTML = 'Este texto se escribió desde la funcion_demo';
+function click_boton_cargar_listado(){
+    //Este es un comentario
+    document.getElementById("divCargarEstudiantes").style="text-align: center; display: block;";
+    document.getElementById("divMenu").style="display: none;";
 }
 
-function click_boton(){
-    funcion_demo();
+
+function click_boton_obtener_participante(){
+    document.getElementById("divObtenerParticipante").style="text-align: center; display: block;";
+    document.getElementById("divMenu").style="display:none";
+}
+
+
+function click_boton_registrar_asistencia(){
+    console.log("***");
+    console.log("*******");
+    console.log("***");
+    console.log("*****");
+    console.log("***********");
+    console.log("*******");
+}
+
+function click_boton_regresar(){
+    document.getElementById("divCargarEstudiantes").style="text-align: center; display: none;";
+    document.getElementById("divObtenerParticipante").style="text-align: center; display: none;";
+    document.getElementById("divMenu").style="display:block";
 }
