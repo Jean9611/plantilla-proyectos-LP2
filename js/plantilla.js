f99c4dcc222545947478ff87f5ef20dc187e6ec0
Juan Pablo Ruiz Guerra <jruizg96.11@gmail.com>
2023-05-22 20:14:46 -0600

    Actualización de clase 22 de mayo 2023

---
+++ js/plantilla.js
@@ -0,0 +1,29 @@
+
+function funcion_demo(){
+    document.getElementById('lblResultado').innerHTML = 'Este texto se escribió desde la funcion_demo';
+}
+
+
+function click_boton(){
+    funcion_demo();
+}
+
+
+function click_boton_cargar_listado(){
+    document.getElementById('divCargarListado').style = "display: block;";
+    document.getElementById('divMenu').style = "display: none;";
+}
+
+
+
+function click_boton_obtener_participante(){
+    document.getElementById('divObtenerParticipante').style = "display: block;";
+    document.getElementById('divMenu').style = "display: none;";
+}
+
+
+function click_boton_registrar_asistencia(){
+    document.getElementById('divRegistrarAsistencia').style = "display: block;";
+    document.getElementById('divMenu').style = "display: none;"
+}
+
